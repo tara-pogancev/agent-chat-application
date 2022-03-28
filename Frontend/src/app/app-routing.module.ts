6ed77d438eebf9386d0c912db1a247278f1cb0bd
Tara Pogančev <tarapogancev@gmail.com>
2022-03-28 11:36:26 +0200

    feat: angular material theme setup

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatPageComponent } from './components/chat-page/chat-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'dashboard', component: ChatPageComponent },
+  { path: '**', component: ErrorPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { ChatPageComponent } from './components/chat-page/chat-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ChatPageComponent,
+    RegisterPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
